6e5c2cc6c83eff1f64b1f40be13addc541a4c8f2
Zgreatch <franktchetta54@gmail.com>
2023-05-29 09:33:21 +0100

    A short video as front-end Demo plus other updates

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,8 +14,8 @@ export const Footer = () => {
         <div className="footer-content">
           <div className="contact-info">
             <h3>Contact Information</h3>
-            <p>Email: example@example.com</p>
-            <p>Phone: +1 123-456-7890</p>
+            <p>Email: dsfghgfgd@gmail.com</p>
+            <p>Phone: +237 672 77 64 67</p>
           </div>
           <div className="references">
             <h3>References</h3>
