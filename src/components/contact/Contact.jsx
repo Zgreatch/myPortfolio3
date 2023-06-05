157e06756c40083e9fcdc6a80478b26cab7c41b7
Zgreatch <franktchetta54@gmail.com>
2023-06-05 01:12:42 +0100

    I am always done with the project

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,3 +1,71 @@
+import React, { useRef } from 'react';
+import './contact.css';
+import { MdOutlineEmail } from 'react-icons/md';
+import { AiOutlineLinkedin } from 'react-icons/ai';
+import { BsWhatsapp } from 'react-icons/bs';
+import axios from 'axios';
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    // Fetch the form data
+    const formData = new FormData(form.current);
+
+    // Make a POST request to the backend API endpoint
+    axios.post('/api/send-email', formData)
+      .then((response) => {
+        console.log(response.data);
+        // Handle any success actions (e.g., show a success message)
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle any error cases (e.g., show an error message)
+      });
+
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get in touch</h5>
+      <h2>Contact me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          {/* Your contact options */}
+          <article className='contact__option'>
+                <MdOutlineEmail className='contact__option__icon'/>
+                <h4>Email</h4>
+                <h5>myEmail@gmail.com</h5>
+                <a href='mailto:franktchetta54@gmail.com' target='_blank'>send a message</a>
+              </article>
+              <article className='contact__option'>
+                <AiOutlineLinkedin className='contact__option__icon'/>
+                <h4>LinkedIn</h4>
+                <h5>Profile</h5>
+                <a href='https://www.linkedin.com/in/brown-djomo-844b96164/' target='_blank'>Tape me on LinkedIn</a>
+              </article>
+              <article className='contact__option'>
+                <BsWhatsapp className='contact__option__icon'/>
+                <h4>Whatsapp</h4>
+                <h5>Direct message</h5>
+                <a href='https://web.whatsapp.com/send?phone=+237672776467' target='_blank'>Whatsapp me</a>
+                </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Full name" required />
+          <input type="email" name="email" placeholder="Your email" required />
+          <textarea name="message" id="message" cols="30" rows="10" placeholder="Your message" required></textarea>
+          <button type="submit" className="btn btn-primary">Send message</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
 /*import React, { useRef } from 'react';
 import './contact.css';
 import { MdOutlineEmail } from 'react-icons/md';
@@ -82,72 +150,3 @@ const Contact = () => {
   )
 }
 export default Contact*/
-
-import React, { useRef } from 'react';
-import './contact.css';
-import { MdOutlineEmail } from 'react-icons/md';
-import { AiOutlineLinkedin } from 'react-icons/ai';
-import { BsWhatsapp } from 'react-icons/bs';
-import axios from 'axios';
-
-const Contact = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    // Fetch the form data
-    const formData = new FormData(form.current);
-
-    // Make a POST request to the backend API endpoint
-    axios.post('/api/send-email', formData)
-      .then((response) => {
-        console.log(response.data);
-        // Handle any success actions (e.g., show a success message)
-      })
-      .catch((error) => {
-        console.error(error);
-        // Handle any error cases (e.g., show an error message)
-      });
-
-    e.target.reset();
-  };
-
-  return (
-    <section id="contact">
-      <h5>Get in touch</h5>
-      <h2>Contact me</h2>
-      <div className="container contact__container">
-        <div className="contact__options">
-          {/* Your contact options */}
-          <article className='contact__option'>
-                <MdOutlineEmail className='contact__option__icon'/>
-                <h4>Email</h4>
-                <h5>myEmail@gmail.com</h5>
-                <a href='mailto:franktchetta54@gmail.com' target='_blank'>send a message</a>
-              </article>
-              <article className='contact__option'>
-                <AiOutlineLinkedin className='contact__option__icon'/>
-                <h4>LinkedIn</h4>
-                <h5>Profile</h5>
-                <a href='https://www.linkedin.com/in/brown-djomo-844b96164/' target='_blank'>Tape me on LinkedIn</a>
-              </article>
-              <article className='contact__option'>
-                <BsWhatsapp className='contact__option__icon'/>
-                <h4>Whatsapp</h4>
-                <h5>Direct message</h5>
-                <a href='https://web.whatsapp.com/send?phone=+237672776467' target='_blank'>Whatsapp me</a>
-                </article>
-        </div>
-        <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="name" placeholder="Full name" required />
-          <input type="email" name="email" placeholder="Your email" required />
-          <textarea name="message" id="message" cols="30" rows="10" placeholder="Your message" required></textarea>
-          <button type="submit" className="btn btn-primary">Send message</button>
-        </form>
-      </div>
-    </section>
-  );
-};
-
-export default Contact;
